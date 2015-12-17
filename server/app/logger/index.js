5db7181c36155e5e2d2035a984eb106355abd3fd
Vitor Fernandes <vmlf01@gmail.com>
2015-12-17 22:03:49 +0000

    add winston logger module

---
+++ server/app/logger/index.js
@@ -0,0 +1,25 @@
+var winston = require('winston');
+var fs = require('fs');
+
+(function ensureLogsDirExists() {
+  fs.existsSync("./logs") || fs.mkdirSync("./logs");
+}());
+
+var logger = new (winston.Logger)({
+  level: 'info',
+  transports: [
+    new (winston.transports.Console)()
+    //new (winston.transports.File)({
+    //  name: 'info-file',
+    //  filename: 'logs/esp-lights.log',
+    //  level: 'info'
+    //}),
+    //new (winston.transports.File)({
+    //  name: 'error-file',
+    //  filename: 'esp-lights.error.log',
+    //  level: 'error'
+    //})
+  ]
+});
+
+module.exports = logger;
