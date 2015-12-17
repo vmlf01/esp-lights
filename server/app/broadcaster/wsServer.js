a05ede348c626f76131716daca4d40fe7edb0d59
Vitor Fernandes <vmlf01@gmail.com>
2015-12-17 22:06:25 +0000

    add event emitter to ws broadcaster

---
+++ server/app/broadcaster/wsServer.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var util = require('util');
+var EventEmitter = require('events');
+var ws = require('ws');
+var url = require('url');
+
+var logger = require('../logger');
+
+function Broadcaster(appServer) {
+
+  EventEmitter.call(this);
+
+  var self = this;
+  var appServer = appServer;
+  var wsServer = null;
+
+  this.start = function () {
+    if (wsServer !== null) {
+      self.stop();
+    }
+
+    wsServer = new ws.Server({server: appServer});
+    wsServer.on('connection', handleWebsocketConnection);
+  }
+
+  this.stop = function () {
+    if (wsServer !== null) {
+      wsServer.removeListener('connection', handleWebsocketConnection);
+      wsServer.close();
+      wsServer = null;
+    }
+  }
+
+  this.broadcastStatus = function (status) {
+    if (wsServer !== null) {
+      broadcastLightsStatus(wsServer.wsClients, status);
+    }
+  }
+
+  this.broadcastStatusToClient = function (client, status) {
+    if (wsServer !== null) {
+      broadcastLightsStatus([client], status);
+    }
+  }
+
+  function handleWebsocketConnection(ws) {
+    try {
+      logger.info('New ws connection', ws.upgradeReq.url);
+      self.emit('connection', ws);
+    }
+    catch (ex) {
+      logger.error('Error handling ws connection', ex);
+    }
+  }
+
+  function broadcastLightsStatus(wsClients, status) {
+
+    var msg = { type: 'status', status: status };
+
+    wsClients.forEach(function (ws) {
+      ws.send(JSON.stringify(msg), function (err) {
+        if (err) {
+          logger.error('Error broadcasting status update', err);
+        }
+      });
+    });
+  }
+}
+
+util.inherits(Broadcaster, EventEmitter);
+
+module.exports = Broadcaster;
---
+++ server/test/broadcaster/wsServer.spec.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var chai = require("chai");
+var should = chai.should();
+chai.use(require('chai-things'));
+
+var http = require('http');
+var ws = require('ws');
+var Broadcaster = require('../../app/broadcaster');
+var wsServer;
+var appServer;
+var testPort = process.env.PORT || 54321;
+
+describe('wsServer', function () {
+  beforeEach(function () {
+    appServer = http.createServer();
+    appServer.listen(54321);
+    wsServer = new Broadcaster(appServer);
+  });
+
+  afterEach(function () {
+    wsServer.stop();
+    appServer.close();
+  });
+
+  describe('module.exports', function () {
+
+    it('should export a server object', function (done) {
+      should.exist(wsServer);
+      wsServer.should.be.an('object');
+
+      done();
+    });
+
+    it('should have all public inteface methods', function (done) {
+      var publicMethods = ['start', 'stop', 'broadcastStatus', 'broadcastStatusToClient'];
+
+      publicMethods.should.all.satisfy(function (f) {
+        return wsServer.should.respondTo(f);
+      });
+
+      done();
+    });
+
+  });
+
+  describe('start', function () {
+
+    it('should start a websocket server', function (done) {
+      wsServer.start.should.not.throw();
+
+      var client = ws.connect('ws://localhost:' + testPort, done);
+    });
+
+    it('should emit connection event when a client connects', function (done) {
+      wsServer.start();
+      wsServer.once('connection', function (ws) {
+        ws.should.be.an('object');
+
+        done();
+      })
+      var client = ws.connect('ws://localhost:' + testPort);
+    });
+
+  });
+
+  describe('stop', function () {
+    it('should stop a websocket server', function (done) {
+
+      wsServer.start();
+
+      var client = ws.connect('ws://localhost:' + testPort);
+      client.once('open', function () {
+        wsServer.stop.should.not.throw();
+      });
+
+      client.once('close', function () {
+        done();
+      });
+
+    });
+
+  });
+
+});
